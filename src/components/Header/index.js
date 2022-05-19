cb71075442fff9dc06dadd2a0a33dd6a8aec7cb3
MargauxPuget <margaux.puget@gmail.com>
2022-05-19 16:49:28 +0200

    mise en place de l'affiche conditionnel des 'nav' de la page projet

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,36 +1,35 @@
-/* eslint-disable react/self-closing-comp */
 import './style.scss';
-import { Link, useLocation } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { Link, Routes, Route } from 'react-router-dom';
 
 function Header() {
-  const sampleLocation = useLocation();
-  console.log(sampleLocation.pathname);
-
-  const isProjectPage = (sampleLocation == "/projets");
-  console.log(isProjectPage);
-
   return (
     <header className="header">
-      {isProjectPage && (
-        <>
-          <aside className="aside aside--left">
-            <h4>Projets</h4>
-            <h5>Passés</h5>
-          </aside>
-          <aside className="aside aside--right">
-            <h4>Projets</h4>
-            <h5>Future</h5>
-          </aside>
-        </>
-      )}
+      <Routes>
+        <Route
+          path="/projets"
+          element={
+            (
+              <>
+                <aside className="aside aside--left">
+                  <h4>Projets</h4>
+                  <h5>Passés</h5>
+                </aside>
+                <aside className="aside aside--right">
+                  <h4>Projets</h4>
+                  <h5>Future</h5>
+                </aside>
+              </>
+            )
+        }
+        />
+      </Routes>
+
       <Link to="/">
         <h1 className="header_title">Margaux Puget</h1>
       </Link>
       <h2 className="header_soustitle">Développeuse en cours de chargement ...</h2>
       <div className="header_progressbar--wrapper">
-        <div className="header_progressbar">
-        </div>
+        <div className="header_progressbar" />
       </div>
     </header>
   );
